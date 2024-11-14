ead5ad04ddc1a98667ed937e04c033b854859b82
spaceGC24 <chminho0224@naver.com>
2024-11-14 20:55:50 +0900

    Update and rename testers.py to tester.py

---
+++ tester.py
@@ -0,0 +1,9 @@
+import geo.utils as uitls
+
+a,b = 3,4
+c = uitls.pythagoras(a,b)
+print('c = ',c)
+
+r = 10
+area = uitls.circle(r)
+print('area = ',area)
--- testers.py
+++
@@ -1,9 +0,0 @@
-import geo.utils as uitls
-
-a,b=3,4
-c=uitls.pythagoras(a,b)
-print('c=',c)
-
-r=10
-area=uitls.circle(r)
-print('area=',area)
