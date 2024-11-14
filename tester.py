84b63c4bc3ac8356ba7555987d94687b16fa530c
spaceGC24 <chminho0224@naver.com>
2024-11-14 20:57:29 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,9 +1,9 @@
 import geo.utils as uitls
 
-a,b = 3,4
-c = uitls.pythagoras(a,b)
+a,b=3,4
+c=uitls.pythagoras(a,b)
 print('c = ',c)
 
-r = 10
-area = uitls.circle(r)
+r=10
+area=uitls.circle(r)
 print('area = ',area)
