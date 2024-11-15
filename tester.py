859ee7aa33c74db8347776b753acdbb2abe9924d
spaceGC24 <chminho0224@naver.com>
2024-11-15 21:31:26 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,9 +1,9 @@
-import geo.utils as uitls
+import geo.utils as utils
 
 a,b = 3,4
-c = uitls.pythagoras(a,b)
+c = utils.pythagoras(a,b)
 print(' c = ', c)
 
 r = 10
-area=uitls.circle(r)
+area=utils.circle(r)
 print(' area = ', area)
